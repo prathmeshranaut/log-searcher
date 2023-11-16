fa4035e2f107a234391f60d16af311ec2718f210
Prathmesh Ranaut <aayush.ranaut@gmail.com>
2023-11-16 17:42:03 -0500

    Created log searcher with REST API

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const SearchRoutes = require('./search/routes.config')
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Log Searcher app is running");
+});
+//Add routes
+SearchRoutes.routesConfig(app);
+
+app.listen(port, () => {
+  console.log(`Log Searcher app listening on port ${port}`);
+});
